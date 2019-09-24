c75b8a52039d2f7286ce0870f058cfcc1fb9554b
happy199 <Tidjani.happy@gmail.com>
2019-09-24 23:08:04 +0100

    Implement ListView with item builder

--- lib/src/screens/new_user_list.dart
+++ lib/src/screens/new_user_list.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
 
-class NewUserList extends StatefulWidget{
+class NewUserList extends StatelessWidget{
   Widget build(context){
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Nos Utilisateurs"),
-      ),
-      body: Text("Ici tous les utilateurs inscrits sur zaxo"),
+      body: buildList(),
     );
   }
-
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return null;
+  Widget buildList(){
+    return ListView.builder(
+      itemCount: 1000,
+      itemBuilder: (context, int index){
+        return FutureBuilder(
+          future: getFuture(),
+          builder: (context, snapshot){
+            return Container(
+              height: 80.0,
+              child: snapshot.hasData
+                ? Text ('$index')
+                : Text ('En cours de chargement $index'),
+            );
+          },
+        );
+      },
+    );
+  }
+  getFuture(){
+    return Future.delayed(
+      Duration(seconds: 2),
+      () => "Hi",
+    );
   }
 }
